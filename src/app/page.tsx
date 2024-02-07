e1449316efaa7cbd301b316ccb6893cf795e6225
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2024-02-07 10:29:45 -0300

    Add hero section to home page and update navigatio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import {Nav} from "@/components/Nav/Nav";
+import Image from "next/image";
+import bgMobileLanding from "../../public/bgMobileLanding.svg"
+import styleBg from "./style.module.css"
 
 export default function Home() {
   return (
-    <Nav/>
+    <>
+        <Nav/>
+        {/* Hero */}
+        <section className={styleBg.backgroundSection}>
+            <div className="relative top-32">
+                <h1 className="font-bold text-bold text-white text-[44px]">Gerencie seu estoque de uma  forma mais eficiente.</h1>
+            </div>
+        </section>
+    </>
   )
 }
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,26 +1,12 @@
-import Link from "next/link";
 import Image from "next/image";
 import logoLanding from "../../../public/logoLanding.svg"
 
 export function Nav() {
 
     return (
-        <div className='bg-primaryColor flex items-center justify-between md:justify-center'>
-            <div className="w-full flex items-center justify-between md:max-w-[1440px] px-4 py-7 md:px-12">
-                <div className='flex items-center justify-between w-full'>
-                    <Link href="/">
-                        <Image className="w-[98px]" src={logoLanding} alt="Logo"/>
-                    </Link>
-
-                    <div className="hidden md:block">
-                        <ul className='flex items-center text-secondaryColor gap-12'>
-                            <li>Serviços</li>
-                            <li>Canais de Comunicação</li>
-                            <li>Nossos Produtos</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+        <div className='flex items-center justify-between px-4 pt-6 absolute w-full'>
+            <Image src={logoLanding} alt="logo" />
+            <button className="text-white bg-white bg-opacity-30 w-40 h-10 rounded-3xl">Vamos começar</button>
         </div>
     )
 }
