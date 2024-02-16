b498fd2af6367f45c5af4ebe8b97625080800603
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2024-02-16 10:43:05 -0300

    feat: add dropdown menu and sorting functionality

--- src/components/ProductsTable/columns.tsx
+++ src/components/ProductsTable/columns.tsx
@@ -1,6 +1,16 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -16,7 +26,17 @@ export type Products = {
 export const columns: ColumnDef<Products>[] = [
     {
         accessorKey: "id",
-        header: "ID",
+        header: ({ column }) => {
+            return (
+              <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+              >
+                Id
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+              </Button>
+            )
+          },
     },
     {
         accessorKey: "name",
@@ -38,4 +58,32 @@ export const columns: ColumnDef<Products>[] = [
         accessorKey: "segment",
         header: "Segmento",
     },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+          const products = row.original
+     
+          return (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                  <span className="sr-only">Open menu</span>
+                  <MoreHorizontal className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="bg-white rounded-xl" align="end">
+                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                <DropdownMenuItem
+                  onClick={() => navigator.clipboard.writeText(products.name)}
+                >
+                  Copy Product ID
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>View customer</DropdownMenuItem>
+                <DropdownMenuItem>View payment details</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )
+        },
+      },
 ]
